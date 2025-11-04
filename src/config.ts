ff4ad187bde669b0bf1bc2278bb8e559b4ccc0a2
Eddie <edward@dcspark.io>
2025-11-03 21:34:59 -0300

    link

--- src/config.ts
+++ src/config.ts
@@ -5,12 +5,12 @@ export const URL_GITHUB = 'https://github.com/PaimaStudios/paima-engine';
 
 export const URL_BUILD_W3_APPS = '/';
 export const URL_CHAINS = '/home/chains/';
-export const URL_SCALABLE_SECURE = '/what-is-paima-engine';
+export const URL_SCALABLE_SECURE = '/what-is-effectstream';
 export const URL_OPEN_SOURCE = URL_GITHUB;
 export const URL_LOCAL_DEVELOPMENT = '/home/components/processes';
 export const URL_MADE_FOR_DEVS = '/quick-start';
 export const URL_PROCESS_MANAGEMENT = '/home/components/processes';
 export const URL_COMPONENTS = '/home/components/';
 export const URL_QUICK_START_GUIDE = '/quick-start';
-export const URL_VISUALIZATION = '/what-is-paima-engine';
+export const URL_VISUALIZATION = '/what-is-effectstream';
 export const URL_EXAMPLES = '/home/templates/multi-chain-swap';
